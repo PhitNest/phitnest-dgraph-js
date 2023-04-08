6e74f45eb11056d027b9af592d2b09f615ff457d
Jp <jpsiegel8@gmail.com>
2023-04-08 18:52:56 -0400

    Compiled types

--- lib/txn.d.ts
+++ lib/txn.d.ts
@@ -6,21 +6,31 @@ export declare class Txn {
     private finished;
     private mutated;
     constructor(dc: DgraphClient, options?: TxnOptions);
-    mutateGraphQL<T, Relationships extends keyof T | undefined = undefined>(mutation: {
+    mutateGraphQL<
+        T,
+        Relationships extends keyof T | undefined = undefined,
+    >(mutation: {
         obj: SchemaType<T, Relationships>;
         commitNow: boolean;
     }): Promise<Assigned>;
     queryGraphQL(query: string): Promise<{
         [k: string]: any[];
     }>;
-    query(q: string, options?: {
-        debug?: boolean;
-    }): Promise<Response>;
-    queryWithVars(q: string, vars?: {
-        [k: string]: any;
-    }, options?: {
-        debug?: boolean;
-    }): Promise<Response>;
+    query(
+        q: string,
+        options?: {
+            debug?: boolean;
+        },
+    ): Promise<Response>;
+    queryWithVars(
+        q: string,
+        vars?: {
+            [k: string]: any;
+        },
+        options?: {
+            debug?: boolean;
+        },
+    ): Promise<Response>;
     mutate(mu: Mutation): Promise<Assigned>;
     commit(): Promise<void>;
     discard(): Promise<void>;
